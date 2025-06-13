57d2dd6f3718696c21d7c86c87a571e51f0747ab
ElliNet13 <elliotmoose4@gmail.com>
2025-06-13 02:23:17 +0100

    Add compare

--- nodes/HashNode/HashNode.node.ts
+++ nodes/HashNode/HashNode.node.ts
@@ -13,7 +13,7 @@ export class HashNode implements INodeType {
 		name: 'hashNode',
 		group: ['transform'],
 		version: 1,
-		description: 'Hash a password with bcrypt',
+		description: 'Hash or compare passwords with bcrypt',
 		defaults: {
 			name: 'Bcrypt Hash',
 		},
@@ -22,12 +22,38 @@ export class HashNode implements INodeType {
 		usableAsTool: true,
 		properties: [
 			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Hash',
+						value: 'hash',
+						description: 'Hash a password',
+						action: 'Hash a password',
+					},
+					{
+						name: 'Compare',
+						value: 'compare',
+						description: 'Compare password against hash',
+						action: 'Compare password against hash',
+					},
+				],
+				default: 'hash',
+			},
+			{
 				displayName: 'Salt Rounds',
 				name: 'saltRounds',
 				type: 'number',
 				default: 10,
 				placeholder: 'Default: 10',
 				description: 'How many salt rounds to apply when hashing the password',
+				displayOptions: {
+					show: {
+						operation: ['hash'],
+					},
+				},
 			},
 			{
 				displayName: 'Plain Text Password',
@@ -36,7 +62,21 @@ export class HashNode implements INodeType {
 				typeOptions: { password: true },
 				default: '',
 				placeholder: 'Enter the plain text password',
-				description: 'The password to be hashed',
+				description: 'The password to hash or compare',
+			},
+			{
+				displayName: 'Hash to Compare',
+				name: 'hashToCompare',
+				type: 'string',
+				typeOptions: { password: true },
+				default: '',
+				placeholder: 'Enter the existing hash',
+				description: 'The bcrypt hash to compare the password against',
+				displayOptions: {
+					show: {
+						operation: ['compare'],
+					},
+				},
 			},
 		],
 	};
@@ -47,24 +87,45 @@ export class HashNode implements INodeType {
 
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
-				const saltRounds = this.getNodeParameter('saltRounds', itemIndex, 10) as number;
+				const operation = this.getNodeParameter('operation', itemIndex) as string;
 				const password = this.getNodeParameter('password', itemIndex, '') as string;
 
 				if (!password) {
 					throw new NodeOperationError(this.getNode(), 'Password cannot be empty.', { itemIndex });
 				}
 
-				const hash = await bcrypt.hash(password, saltRounds);
+				if (operation === 'hash') {
+					const saltRounds = this.getNodeParameter('saltRounds', itemIndex, 10) as number;
 
-				const newItem: INodeExecutionData = {
-					json: {
-						...items[itemIndex].json,
-						hashedPassword: hash,
-					},
-				};
+					const hash = await bcrypt.hash(password, saltRounds);
 
-				returnData.push(newItem);
+					const newItem: INodeExecutionData = {
+						json: {
+							...items[itemIndex].json,
+							hashedPassword: hash,
+						},
+					};
 
+					returnData.push(newItem);
+
+				} else if (operation === 'compare') {
+					const hashToCompare = this.getNodeParameter('hashToCompare', itemIndex, '') as string;
+
+					if (!hashToCompare) {
+						throw new NodeOperationError(this.getNode(), 'Hash to compare cannot be empty.', { itemIndex });
+					}
+
+					const isMatch = await bcrypt.compare(password, hashToCompare);
+
+					const newItem: INodeExecutionData = {
+						json: {
+							...items[itemIndex].json,
+							passwordMatches: isMatch,
+						},
+					};
+
+					returnData.push(newItem);
+				}
 			} catch (error) {
 				if (this.continueOnFail()) {
 					returnData.push({ json: { error: (error as Error).message }, pairedItem: itemIndex });
